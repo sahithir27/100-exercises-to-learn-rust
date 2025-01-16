584442cd3ca693b077c0f2cc37bb7c485eaf9d17
Sahithi Gaddam <gaddam.sahi@northeastern.edu>
2025-01-16 17:39:41 -0500

    Implemented source for InvalidStatus using thiserror

--- exercises/05_ticket_v2/14_source/src/lib.rs
+++ exercises/05_ticket_v2/14_source/src/lib.rs
@@ -1,5 +1,4 @@
-use crate::status::Status;
-
+use crate::status::{ParseStatusError, Status};
 // We've seen how to declare modules in one of the earliest exercises, but
 // we haven't seen how to extract them into separate files.
 // Let's fix that now!
@@ -23,6 +22,11 @@ pub enum TicketNewError {
     DescriptionCannotBeEmpty,
     #[error("Description cannot be longer than 500 bytes")]
     DescriptionTooLong,
+    #[error("{0}")]
+    InvalidStatus(
+        #[from]
+        ParseStatusError
+    )
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -48,6 +52,7 @@ impl Ticket {
         }
 
         // TODO: Parse the status string into a `Status` enum.
+        let status=Status::try_from(status)?;
 
         Ok(Ticket {
             title,
