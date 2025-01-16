302c00bfac687ab38c65641ba123461be5389060
Sahithi Gaddam <gaddam.sahi@northeastern.edu>
2025-01-16 17:28:34 -0500

    Implemented TryFrom<String> and TryFrom<&str> for Status

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -8,6 +8,31 @@ enum Status {
     Done,
 }
 
+impl TryFrom<String> for Status{
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        // match value.to_lowercase().as_str() {
+        //     "todo"=>Ok(Status::ToDo),
+        //     "inprogress"=>Ok(Status::InProgress),
+        //     "done"=>Ok(Status::Done),
+        //     _=>Err("Invalid String".into())
+        // }
+        value.as_str().try_into()
+    }
+}
+
+impl TryFrom<&str> for Status{
+    type Error = String;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo"=>Ok(Status::ToDo),
+            "inprogress"=>Ok(Status::InProgress),
+            "done"=>Ok(Status::Done),
+            _=>Err("Invalid String".into())
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
