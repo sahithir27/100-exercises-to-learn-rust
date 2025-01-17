ee9834d3afe18c76fea6c3414db98d073d6cd6a9
Sahithi Gaddam <gaddam.sahi@northeastern.edu>
2025-01-17 14:02:32 -0500

    Implemented Fibonacci function using Vec

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,14 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    // let mut nums:Vec<u32>=Vec::with_capacity(n as usize+1);
+    // nums.push(0);
+    // nums.push(1);
+    let mut nums:Vec<u32>=vec![0,1];
+    for i in 2..=n{
+        nums.push(nums[i as usize-1]+nums[i as usize-2])
+    }
+    nums[n as usize]
 }
 
 #[cfg(test)]
