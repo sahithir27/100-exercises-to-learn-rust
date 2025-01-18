bebc17d3fc06cd827b13e9124284d2d915f72724
Sahithi Gaddam <gaddam.sahi@northeastern.edu>
2025-01-17 21:47:14 -0500

    Implemented Index<TicketId>  and Index<&TicketId> for TicketStore

--- exercises/06_ticket_management/13_index/src/lib.rs
+++ exercises/06_ticket_management/13_index/src/lib.rs
@@ -1,7 +1,7 @@
 // TODO: Implement `Index<&TicketId>` and `Index<TicketId>` for `TicketStore`.
 
 use ticket_fields::{TicketDescription, TicketTitle};
-
+use std::ops::Index;
 #[derive(Clone)]
 pub struct TicketStore {
     tickets: Vec<Ticket>,
@@ -55,6 +55,20 @@ impl TicketStore {
 
     pub fn get(&self, id: TicketId) -> Option<&Ticket> {
         self.tickets.iter().find(|&t| t.id == id)
+    }
+}
+
+impl Index<TicketId> for TicketStore{
+    type Output = Ticket;
+    fn index(&self, index:TicketId)->&Self::Output{
+        self.get(index).unwrap()
+    }
+}
+
+impl Index<&TicketId> for TicketStore{
+    type Output = Ticket;
+    fn index(&self, index:&TicketId)->&Self::Output{
+        &self[*index]
     }
 }
 
